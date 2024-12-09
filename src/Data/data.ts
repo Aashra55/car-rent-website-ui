6966ee50deddef705bdf12b301f97766df2d6174
Aashra <saleemlatif17@gmail.com>
2024-12-09 13:50:54 +0500

    first push

---
+++ src/Data/data.ts
@@ -0,0 +1,4 @@
+import { Popular as SectionA} from "@/app/card-data/page";
+import { Recommendation as SectionB } from "@/app/card-data/page";
+export const sectionA = SectionA;
+export const sectionB = SectionB;
